1822e750ba926fbe1d2601e00323cf6509c16519
Akhila Nenavath <akhila.nenavath@zemosolabs.com>
2024-09-19 22:57:10 +0530

    Feat: Add Components

---
+++ sonar-cloud/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import CardContain from "./components/organism/CardContain";
+
+function App() {
+  return (
+    <div className="App">
+      <CardContain />
+    </div>
+  );
+}
+
+export default App;
