7306bd00e5ceb2231394c6a8631059677e4979f8
Akhila Nenavath <akhila.nenavath@zemosolabs.com>
2024-09-20 15:50:06 +0530

    Feat: Add test file

--- sonar-cloud/src/App.tsx
+++ sonar-cloud/src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import CardContain from "./components/organism/CardContain";
+import { Typography } from "@mui/material";
 
 function App() {
   return (
     <div className="App">
       <CardContain />
+      <Typography>learn react</Typography>
     </div>
   );
 }
