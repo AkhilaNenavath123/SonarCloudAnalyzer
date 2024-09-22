a9d91fd688db071b009cb0f06f5191be542cae73
Akhila Nenavath <akhila.nenavath@zemosolabs.com>
2024-09-22 11:47:23 +0530

    Fix: Add props for button

--- sonar-cloud/src/App.tsx
+++ sonar-cloud/src/App.tsx
@@ -1,14 +1,23 @@
 import React from "react";
-import "./App.css";
 import SimpleButton from "./components/Button";
-import { Typography } from "@mui/material";
+import { Typography, Box } from "@mui/material";
 
 function App() {
   return (
-    <div className="App">
-      <SimpleButton />
-      <Typography>learn react</Typography>
-    </div>
+    <Box
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      height="100vh"
+      flexDirection="column"
+    >
+      <SimpleButton variant="contained" color="primary">
+        Click Me
+      </SimpleButton>
+      <Typography variant="h6" mt={2}>
+        Learn React
+      </Typography>
+    </Box>
   );
 }
 
